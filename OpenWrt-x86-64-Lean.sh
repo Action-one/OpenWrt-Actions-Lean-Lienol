ab890e9b34f86eb05bc2c9c8dfbd1304360fabb6
尐ˊ;栤嚸┌ <20243226+kang-mk@users.noreply.github.com>
2020-04-03 20:27:55 +0800

    Update OpenWrt-x86-64-Lean.sh

--- OpenWrt-x86-64-Lean.sh
+++ OpenWrt-x86-64-Lean.sh
@@ -126,7 +126,7 @@ EOF
 # 第三方插件选择:
 cat >> .config <<EOF
 # CONFIG_PACKAGE_luci-app-vssr=y #Hello World(懒得折腾,未验证是否正确,暂不开启)
-# CONFIG_PACKAGE_luci-app-oaf=y #应用过滤
+CONFIG_PACKAGE_luci-app-oaf=y #应用过滤
 # CONFIG_PACKAGE_luci-app-openclash=y #OpenClash
 CONFIG_PACKAGE_luci-app-serverchan=y #微信推送
 # CONFIG_PACKAGE_luci-app-adguardhome=y #ADguardhome
