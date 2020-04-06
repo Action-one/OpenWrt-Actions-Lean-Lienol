fec85a1417156d636e94d9e3fc82945dd3055201
尐ˊ;栤嚸┌ <20243226+kang-mk@users.noreply.github.com>
2020-04-06 10:37:38 +0800

    Update OpenWrt-x86-64-Lean.sh

--- OpenWrt-x86-64-Lean.sh
+++ OpenWrt-x86-64-Lean.sh
@@ -171,6 +171,7 @@ CONFIG_PACKAGE_luci-app-autoreboot=y #定时重启
 CONFIG_PACKAGE_luci-app-upnp=y #通用即插即用UPnP(端口自动转发)
 CONFIG_PACKAGE_luci-app-accesscontrol=y #上网时间控制
 CONFIG_PACKAGE_luci-app-wol=y #网络唤醒
+CONFIG_PACKAGE_luci-app-sqm=y #SQM智能队列管理
 CONFIG_PACKAGE_luci-app-flowoffload=y #Turbo ACC 网络加速
 CONFIG_PACKAGE_luci-app-softethervpn=y #SoftEtherVPN服务器
 CONFIG_PACKAGE_luci-app-haproxy-tcp=y #Haproxy负载均衡
