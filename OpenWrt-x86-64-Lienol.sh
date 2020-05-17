403dad6004f41de225a142e2d21fe5d73e68f86e
尐ˊ;栤嚸┌ <20243226+kang-mk@users.noreply.github.com>
2020-05-17 22:44:42 +0800

    up

--- OpenWrt-x86-64-Lienol.sh
+++ OpenWrt-x86-64-Lienol.sh
@@ -123,7 +123,7 @@ cat >> .config <<EOF
 CONFIG_PACKAGE_luci-app-oaf=y #应用过滤
 CONFIG_PACKAGE_luci-app-openclash=y #OpenClash客户端
 CONFIG_PACKAGE_luci-app-serverchan=y #微信推送
-# CONFIG_PACKAGE_luci-app-eqos=y #IP限速
+CONFIG_PACKAGE_luci-app-eqos=y #IP限速
 EOF
 
 # Passwall插件:
